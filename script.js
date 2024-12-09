330fe09b4effb79d0b2af8fc60f6ad698f9db021
Venght <bvenght@gmail.com>
2024-12-09 20:43:27 +0800

    Minor changes in function playRound, the PlayRound function is used for evluating winners between human and computer battle of rock, paper, and scissors

--- script.js
+++ script.js
@@ -5,34 +5,51 @@ const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice("rock", "paper", "scissors");
 
 function getComputerChoice(rstance, pstance, sstance) {
-    let x = Math.floor(Math.random()* 3) + 1;
-    //console.log(x);
-    if (x===1) {
-        console.log(rstance);
-    }
-
-    else if (x===2) {
-        console.log(pstance);
-    } 
-    
-    else if (x===3) {
-        console.log(sstance);
-    }
-    return;
-
+  let x = Math.floor(Math.random() * 3) + 1;
+  //console.log(x);
+  if (x === 1) {
+    choice = rstance;
+  } else if (x === 2) {
+    choice = pstance;
+  } else if (x === 3) {
+    choice = sstance;
+  }
+  console.log(`Computer choice: ${choice}`)
+  return choice;
 }
 
-function getHumanChoice(){
-    let choice = prompt("Rock, Paper, Scissors?");
-    choice = choice.toLowerCase();
-    return choice;
+function getHumanChoice() {
+  let choice = prompt("Rock, Paper, Scissors?");
+  choice = choice.toLowerCase();
+  return choice;
 }
 
 function playRound(HumanChoice, ComputerChoice) {
-    console.log("Your score is: " + humanScore + " The enemy score is: " + computerScore);
-    return;
+  console.log("Your score is: " + humanScore + " The enemy score is: " + computerScore);
+  if (
+    (HumanChoice == "rock" && ComputerChoice == "scissors") ||
+    (HumanChoice == "paper" && ComputerChoice == "rock") ||
+    (HumanChoice == "scissors" && ComputerChoice == "paper")
+  ) {
+    console.log("You won");
+    humanScore++;
+    
+  } else if (
+    (ComputerChoice == "rock" && HumanChoice == "scissors") ||
+    (ComputerChoice == "paper" && HumanChoice == "rock") ||
+    (ComputerChoice == "scissors" && HumanChoice == "paper")
+  ) {
+    console.log("You lost");
+    computerScore++;
+  } else {
+    console.log("Both are tie");
+  }
+  return;
+}
+
+function playGame(){
+    
 }
 
 playRound(humanSelection, computerSelection);
-console.log("You chose: " , humanSelection);
-
+console.log("You chose: ", humanSelection);
